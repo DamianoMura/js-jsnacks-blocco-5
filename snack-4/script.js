c78198c8c1e62b115376a62cbab5e08f51011888
DamianoMura <mura.damiano14@gmail.com>
2025-07-17 17:12:39 +0200

    [add] snack 4 completed

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+let authors=[];
+posts.forEach((post) => {
+  authors.push(post.author)
+})
+
+console.log(authors)
