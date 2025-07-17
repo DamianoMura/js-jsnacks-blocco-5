18b7cdaf763c35322edb72b329cb1ad2a7a743ad
DamianoMura <mura.damiano14@gmail.com>
2025-07-17 17:05:38 +0200

    [add] snack 3 completed

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,12 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+let newNumbers=[];
+
+numbers.forEach((number) => {
+  newNumbers.push(number+1)
+})
+
+console.log(numbers)
+console.log(newNumbers)
